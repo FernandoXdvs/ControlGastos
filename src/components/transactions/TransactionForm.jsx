71efede4711f5c751df7686221ad5dc2b89aa68c
Fernando Jp <fernandojoaquinpereyda@gmail.com>
2024-09-11 09:54:40 -0600

    Elaboración de la estructura del form

---
+++ src/components/transactions/TransactionForm.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { useGlobalState } from '../../context/GlobalState';
+
+const TransactionForm = () => {
+    const { addTransaction } = useGlobalState();
+    const [description, setDescription] = useState();
+    const [amount, setAmount] = useState(0);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        addTransaction({
+            id: window.crypto.randomUUID(),
+            description,
+            amount,
+        });
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input type="text" placeholder="Ingresa una descripcion"
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+                <input type="number" step="0.01" placeholder="00.00"
+                    onChange={(e) => setAmount(e.target.value)}
+                />
+                <button>
+                    Añadir Transacción
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export { TransactionForm };
